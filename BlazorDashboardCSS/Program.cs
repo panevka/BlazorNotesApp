7daac54af19621cf727ec78747829a27b8f44dbb
panevka <97028404+panevka@users.noreply.github.com>
2025-06-27 16:55:30 +0200

    feat(DashboardCSS): add local storage service to app builder

--- BlazorDashboardCSS/Program.cs
+++ BlazorDashboardCSS/Program.cs
@@ -1,10 +1,14 @@
 using BlazorDashboardCSS.Components;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazoredLocalStorage();
+
 
 var app = builder.Build();
 
